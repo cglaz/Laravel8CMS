e61aae456c1ba141ef7a4648c14ed9a6ca4fd141
cglaz <cezazzr@gmail.com>
2021-02-21 16:58:06 +0100

    Redirect to admin panel after succesful proccess of loging and made logout function

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,6 +1,11 @@
 <x-admin-master>
 
     @section('content')
+
+
+<!-- Begin Page Content -->
+<div class="container-fluid">
+<!-- Page Heading -->
     <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
 
     @endsection
